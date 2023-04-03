23cc06454624c84bbff7e3c735ca2956ac349e5e
Crafterdark <118601009+Crafterdark@users.noreply.github.com>
2023-04-03 14:35:56 +0200

    Added No Dash mod implementation.

--- osu.Game.Rulesets.Catch/Mods/CatchModNoDash.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModNoDash.cs
@@ -1,15 +1,18 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
 using osu.Game.Beatmaps;
+using osu.Game.Configuration;
 using osu.Game.Rulesets.Catch.Beatmaps;
 using osu.Game.Rulesets.Catch.Objects;
 using osu.Game.Rulesets.Catch.UI;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.UI;
 using System;
+using System.Linq;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
@@ -21,6 +24,22 @@ namespace osu.Game.Rulesets.Catch.Mods
         public override double ScoreMultiplier => 1;
         public override IconUsage? Icon => FontAwesome.Solid.Moon; //Placeholder
         public override ModType Type => ModType.Conversion;
+
+        [SettingSource("ExGon Mode", "No Dash patterns are catchable to the very pixel limit.")] //Temporarily :)
+        public BindableBool NoDashPatternLimit { get; } = new BindableBool(false);
+
+        public override string SettingDescription
+        {
+            get
+            {
+                string noDashPatternLimit = NoDashPatternLimit.IsDefault ? string.Empty : string.Empty;
+                return string.Join(", ", new[]
+                {
+                    base.SettingDescription,
+                    noDashPatternLimit,
+                }.Where(s => !string.IsNullOrEmpty(s)));
+            }
+        }
 
         public override Type[] IncompatibleMods => new[] { typeof(CatchModAlwaysDash) };
 
@@ -42,6 +61,7 @@ namespace osu.Game.Rulesets.Catch.Mods
         {
             var catchProcessor = (CatchBeatmapProcessor)beatmapProcessor;
             catchProcessor.NoDashHyperOffsets = true;
+            catchProcessor.NoDashPatternLimit = NoDashPatternLimit.Value;
         }
 
     }
