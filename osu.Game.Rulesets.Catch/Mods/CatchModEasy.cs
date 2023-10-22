f7d8195819b2d24659b19973f5916705d3cee997
Crafterdark <118601009+Crafterdark@users.noreply.github.com>
2023-10-22 02:47:44 +0200

    Added Spicy Patterns to EZ mod. Added extra option to SD mod.

--- osu.Game.Rulesets.Catch/Mods/CatchModEasy.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModEasy.cs
@@ -1,13 +1,26 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Framework.Localisation;
+using osu.Game.Beatmaps;
+using osu.Game.Configuration;
+using osu.Game.Rulesets.Catch.Beatmaps;
 using osu.Game.Rulesets.Mods;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
-    public class CatchModEasy : ModEasyWithExtraLives
+    public class CatchModEasy : ModEasyWithExtraLives, IApplicableToBeatmapProcessor
     {
         public override LocalisableString Description => @"Larger fruits, more forgiving HP drain, less accuracy required, and three lives!";
+
+        [SettingSource("Spicy Patterns", "Adjust the patterns as if Hard Rock is enabled.")]
+        public BindableBool HardRockOffsets { get; } = new BindableBool();
+
+        public void ApplyToBeatmapProcessor(IBeatmapProcessor beatmapProcessor)
+        {
+            var catchProcessor = (CatchBeatmapProcessor)beatmapProcessor;
+            catchProcessor.HardRockOffsets = HardRockOffsets.Value;
+        }
     }
 }
--- osu.Game.Rulesets.Catch/Mods/CatchModSuddenDeath.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModSuddenDeath.cs
@@ -1,11 +1,22 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
+using osu.Game.Configuration;
+using osu.Game.Rulesets.Catch.Objects;
+using osu.Game.Rulesets.Judgements;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
     public class CatchModSuddenDeath : ModSuddenDeath
     {
+        [SettingSource("Do not fail by droplets", "Do not restart when missing droplets.")]
+        public BindableBool DropletsCannotFail { get; } = new BindableBool(false);
+
+        protected override bool FailCondition(HealthProcessor healthProcessor, JudgementResult result)
+    => result.Type.AffectsCombo()
+       && !result.IsHit && (!DropletsCannotFail.Value || result.HitObject is not Droplet);
     }
 }
