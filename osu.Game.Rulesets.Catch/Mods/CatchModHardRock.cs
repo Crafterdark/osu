15117d5043fc238aee87f1b99143d8a20633e8a8
Crafterdark <118601009+Crafterdark@users.noreply.github.com>
2024-01-01 14:31:28 +0100

    Added Remove Spicy Patterns setting for CatchModHardRock.

--- osu.Game.Rulesets.Catch/Mods/CatchModHardRock.cs
+++ osu.Game.Rulesets.Catch/Mods/CatchModHardRock.cs
@@ -5,6 +5,8 @@ using System;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Catch.Beatmaps;
+using osu.Framework.Bindables;
+using osu.Game.Configuration;
 
 namespace osu.Game.Rulesets.Catch.Mods
 {
@@ -12,10 +14,13 @@ namespace osu.Game.Rulesets.Catch.Mods
     {
         public override double ScoreMultiplier => UsesDefaultConfiguration ? 1.12 : 1;
 
+        [SettingSource("Remove Spicy Patterns", "Removes random offsets applied by the mod.")]
+        public Bindable<bool> RemoveHardRockOffsets { get; } = new BindableBool(false);
+
         public void ApplyToBeatmapProcessor(IBeatmapProcessor beatmapProcessor)
         {
             var catchProcessor = (CatchBeatmapProcessor)beatmapProcessor;
-            catchProcessor.HardRockOffsets = true;
+            catchProcessor.HardRockOffsets = !RemoveHardRockOffsets.Value;
         }
 
         public override void ApplyToDifficulty(BeatmapDifficulty difficulty)
