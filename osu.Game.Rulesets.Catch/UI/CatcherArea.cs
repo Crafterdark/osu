a3874d2567dd6a96b5e2d23dc899de74d411d2b8
Crafterdark <118601009+Crafterdark@users.noreply.github.com>
2024-01-22 11:17:16 +0100

    WIP 5 Removed useless code from another class

--- osu.Game.Rulesets.Catch/UI/CatcherArea.cs
+++ osu.Game.Rulesets.Catch/UI/CatcherArea.cs
@@ -6,7 +6,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Input.Bindings;
 using osu.Framework.Input.Events;
-using osu.Framework.Logging;
 using osu.Game.Rulesets.Catch.Objects.Drawables;
 using osu.Game.Rulesets.Catch.Replays;
 using osu.Game.Rulesets.Judgements;
